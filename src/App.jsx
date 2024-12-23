cbee6698f358b028c9ed97ca8ef2fd9b171d4a99
hutleangchhun <hutleangchhun@gmail.com>
2024-12-23 20:49:38 +0700

    Add google font and background

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='flex justify-center items-center h-screen'>
+        <p className='text-green-600 text-4xl font-medium'>Nice To Meet You!</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)} className='bg-red-600 text-white p-10'>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
