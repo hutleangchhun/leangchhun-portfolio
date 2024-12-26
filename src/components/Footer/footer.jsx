1c94566bd590301c6ea8a102c7146d6f4868689d
hutleangchhun <hutleangchhun@gmail.com>
2024-12-26 15:35:25 +0700

    Add footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="w-full mx-auto h-24 bg-gray-900 bg-opacity-30 shadow-md flex justify-center items-center">
-      <div className="text-center text-gray-300 text-base">
+      <div className="text-center text-gray-300 text-sm sm:text-sm md:text-base">
         <p>&copy; 2024 Leangchhun Hut. All rights reserved.</p>
       </div>
     </footer>
