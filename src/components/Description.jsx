b30caef456d8fadb90807e0305211e294d72879e
hutleangchhun <hutleangchhun@gmail.com>
2025-08-06 15:02:20 +0700

    update feat

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -11,6 +11,7 @@ const FreelancerDescription = () => {
           <p className="text-gray-300 text-base sm:text-base md:text-xl max-w-3xl mx-auto opacity-100">
             Are you looking for a website to improve your business? I help
             create responsive and user-friendly websites for your needs.
+            Hello.
           </p>
           <SocialCard />
         </div>
