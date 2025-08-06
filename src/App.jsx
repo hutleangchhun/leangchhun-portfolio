de60ca67eebb0ebf77aa655abe14eaf66e9e1509
hutleangchhun <hutleangchhun@gmail.com>
2025-08-06 14:46:01 +0700

    feat: update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import React, { useEffect, useRef } from "react";
 import "./App.css";
-import ProjectCard from "./components/Card/ProjectCard";
 import Description from "./components/Description";
-import Footer from "./components/Footer/footer";
-import Service from "./components/OurService/Service";
-import Skill from "./components/Skill/Skill";
 
 function App() {
   return (
     <>
       <div className="gradient-background">
         <Description />
-        <Skill />
-        <ProjectCard />
-        <Footer />
       </div>
     </>
   );
